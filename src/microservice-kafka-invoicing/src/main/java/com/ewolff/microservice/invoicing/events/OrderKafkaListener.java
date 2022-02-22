554568dcaed2e65c34c2bad3b1723a126a848d4c
Hannah Troisi <htroisi@pixielabs.ai>
2022-02-22 13:38:19 -0800

    Control invoicing delay with a flag set by USR signals.
    Signed-off-by: Hannah Troisi <htroisi@pixielabs.ai>

--- src/microservice-kafka-invoicing/src/main/java/com/ewolff/microservice/invoicing/events/OrderKafkaListener.java
+++ src/microservice-kafka-invoicing/src/main/java/com/ewolff/microservice/invoicing/events/OrderKafkaListener.java
@@ -9,8 +9,12 @@ import org.springframework.stereotype.Component;
 import com.ewolff.microservice.invoicing.Invoice;
 import com.ewolff.microservice.invoicing.InvoiceService;
 
+import sun.misc.Signal;
+
 @Component
 public class OrderKafkaListener {
+
+	private Boolean delayFlag = false;
 
 	private final Logger log = LoggerFactory.getLogger(OrderKafkaListener.class);
 
@@ -19,17 +23,29 @@ public class OrderKafkaListener {
 	public OrderKafkaListener(InvoiceService invoiceService) {
 		super();
 		this.invoiceService = invoiceService;
+
+		Signal.handle(new Signal("USR1"), signal -> {
+			delayFlag = true;
+			log.info("Received USR1 signal: invoice delay enabled");
+        });
+		Signal.handle(new Signal("USR2"), signal -> {
+			delayFlag = false;
+			log.info("Received USR2 signal: invoice delay disabled");
+        });
 	}
 
 	@KafkaListener(topics = "order")
 	public void order(Invoice invoice, Acknowledgment acknowledgment) {
+
 		log.info("Received invoice " + invoice.getId());
 		invoiceService.generateInvoice(invoice);
+			if (delayFlag) {
                 try {
                     Thread.sleep(1100);
                 } catch(InterruptedException ex) {
                     Thread.currentThread().interrupt();
                 }
+			}
 		acknowledgment.acknowledge();
 	}
 
