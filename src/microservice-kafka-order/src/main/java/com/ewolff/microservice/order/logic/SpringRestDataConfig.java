aa8b39f73897fea3c0e648a13db343c0f4e46be2
Phillip Kuznetsov <pkuznetsov@pixielabs.ai>
2022-11-01 16:37:25 -0700

    Revert "Update loadgen"
    This reverts commit 35355d6acee54032c046161db2f87db3792a1732.
    
    Signed-off-by: Phillip Kuznetsov <pkuznetsov@pixielabs.ai>

--- src/microservice-kafka-order/src/main/java/com/ewolff/microservice/order/logic/SpringRestDataConfig.java
+++ src/microservice-kafka-order/src/main/java/com/ewolff/microservice/order/logic/SpringRestDataConfig.java
@@ -4,15 +4,23 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurerAdapter;
 
 import com.ewolff.microservice.order.customer.Customer;
 import com.ewolff.microservice.order.item.Item;
 
 @Configuration
-public class SpringRestDataConfig implements RepositoryRestConfigurer {
-	@Override
-	public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
-		config.exposeIdsFor(Order.class, Item.class, Customer.class);	
+class SpringRestDataConfig extends RepositoryRestConfigurerAdapter {
+
+	@Bean
+	public RepositoryRestConfigurer repositoryRestConfigurer() {
+
+		return new RepositoryRestConfigurerAdapter() {
+			@Override
+			public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
+				config.exposeIdsFor(Order.class, Item.class, Customer.class);
+			}
+		};
 	}
+
 }
